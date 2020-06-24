21a5062e4e404cb292511e19f1f39e285f9c3402
avrobullet <douglasa@unbc.ca>
2020-06-23 21:35:25 -0700

    Updates to time conversion

--- DataUsage/Raw_Time_Conversion.py
+++ DataUsage/Raw_Time_Conversion.py
@@ -1,51 +1,69 @@
-# Variables
+# GLobal Variables
 import re as reggie
+
 class TimeConversion():
     # Initialization
-    def __init__(self, raw_time=None):
-        self.raw_time = raw_time
-        self.time_formats = {'hours': 24, 'minutes': 60, 'seconds': 60, 'miliseconds': 1000}
+    def __init__(self, user_input=None):
+        # Class Variables
+        self.time_formats = [24, 60, 60, 1000]  # In order: hours=24; minutes=60; seconds=60; miliseconds=1000
         self.time_converted = {'hours': 00, 'minutes': 00, 'seconds': 00, 'miliseconds': 00}
-    # Converting raw times into their respective (and readable) formats
-    def timeConversion(self, new_raw_time=None):
-        # In the event that the submitted raw time was incorret and needs to be replaced
+        self.raw_time = 0 # For unit test purposes (see
+        self.input = user_input
+
+        # In the event of a unit test
+        try:
+            self.raw_time = int(self.input)
+        except ValueError:
+            if self.input == 'unit test':
+                self.timeFormalFormat(9145890)  # Convert designated 9145890 miliseconds to 2hours:32minutes:25seconds:889Ms
+                self.display('Unit test Class TimeConversion!')  # Determine if conversion works
+            else:
+                print('Invalid Request!')
+
+    # Converting milisecond times into HH:MM:SS:MsMs format
+    def timeFormalFormat(self, new_raw_time=None):
+        # In the event that the submitted raw time was incorrect and needs to be replaced
         if new_raw_time != None: self.raw_time = new_raw_time
 
-        # Set a temporary variable
-        tempt_time = str((1 / self.time_formats['minutes']) \
-                         * (1 / self.time_formats['seconds']) \
-                         * (1 / self.time_formats['miliseconds']) \
+        # Set a temporary variable (converted to hours for further processing)
+        tempt_time = str((1 / self.time_formats[1]) \
+                         * (1 / self.time_formats[2]) \
+                         * (1 / self.time_formats[3]) \
                          * self.raw_time)
 
-        # Split to save formatted hours
-        hours, minutes = reggie.split("[.]", tempt_time)
-        self.time_converted['hours'] = hours
-        minutes = '0.' + minutes
+        # Split, calculate, and save formatted time stamps (hours, minutes, seconds, miliseconds
+        iterate = 0
+        for key in self.time_converted.keys():
+            if iterate == 0:
+                self.time_converted[key], next_tempt_time = reggie.split("[.]", tempt_time)
+            elif iterate > 0:
+                tempt_time = str(float('0.' + next_tempt_time) * self.time_formats[iterate])
+                self.time_converted[key],next_tempt_time = reggie.split("[.]", tempt_time)
+            iterate+=1
 
-        # Calculate and split to convert and save minutes
-        tempt_time = str(float(minutes) * self.time_formats['minutes'])
-        minutes, seconds = reggie.split("[.]", tempt_time)
-        self.time_converted['minutes'] = minutes
-        seconds = '0.' + seconds
-
-        # Calculate and split to convert and save seconds
-        tempt_time = str(float(seconds) * self.time_formats['seconds'])
-        seconds, miliseconds = reggie.split("[.]", tempt_time)
-        self.time_converted['seconds'] = seconds
-        miliseconds = '0.' + miliseconds
-
-        # Calculate and split to convert and save minutes
-        tempt_time = str(float(miliseconds) * self.time_formats['miliseconds'])
-        miliseconds, _ = reggie.split("[.]", tempt_time)
-        self.time_converted['miliseconds'] = miliseconds
+    # Converting HH:MM:SS:MsMs time into miliseconds
+    def milisecondRawFormat(self, new_raw_time=None):
+        pass
 
     # Display converted time
-    def timeDisplay(self):
-        print('Current time:', end=' ')
-        for times in self.time_converted:
-            print(self.time_converted[times], times, end=' ')
+    def display(self, quote=None):
+        # What called for a unit test, have the user check that the calculations match the expected result
+        if quote != None:
+            print(quote)
+            print('--> Knowing that {} miliseconds is: 2 hours 32 minutes 25 seconds 889 miliseconds'.format(self.raw_time))
+            print('--> The calculations should matche it:   ', end='')
+        else:
+            print('Current time:', end=' ')
 
-entered_time = int(input("Enter raw time:"))
-convert_time = TimeConversion(entered_time)
-convert_time.timeConversion()
-convert_time.timeDisplay()
+        for key,value in self.time_converted.items():
+            print(value,key, end=' ')
+
+user_input = input("Enter raw time:")
+convert_time = TimeConversion(user_input)
+try:
+    if convert_time.input != 'unit test':
+        if int(convert_time.input):
+            convert_time.timeFormalFormat()
+            convert_time.display()
+except ValueError:
+    pass
